1a755bdfc5f71b59ac0b918f1c6a1873f7a7abc4
himshim26 <mynameishimshim26@gmail.com>
2026-01-11 20:22:44 +0530

    Template optimization

--- js/template-engine.js
+++ js/template-engine.js
@@ -1,5 +1,17 @@
-window.renderTemplate = function (templateHTML, data) {
-  return templateHTML.replace(/{{(.*?)}}/g, (_, key) => {
-    return data[key.trim()] ?? "";
+function renderTemplate(template, data) {
+  let html = template;
+
+  // Replace normal placeholders
+  Object.keys(data).forEach(key => {
+    const value = data[key] ?? "";
+    html = html.replaceAll(`{{${key}}}`, value);
   });
-};
+
+  // 🔥 CONDITIONAL BLOCK SUPPORT
+  html = html.replace(
+    /\{\{#if ([^}]+)\}\}([\s\S]*?)\{\{\/if\}\}/g,
+    (match, key, content) => data[key]?.toString().trim() ? content : ""
+  );
+
+  return html;
+}
